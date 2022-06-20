6b0d853ae4b97dd04dfe99d588c103c7c683bbb9
kushandesilva59 <kushandesilva801@gmail.com>
2022-06-20 15:29:05 +0530

    create crudDao interface

---
+++ src/DAO/CrudDao.java
@@ -0,0 +1,21 @@
+package DAO;
+
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface CrudDao <T,ID> {
+    ArrayList<T> getAll() throws SQLException, ClassNotFoundException;
+
+    boolean save(T dto) throws SQLException, ClassNotFoundException;
+
+    boolean update(T dto) throws SQLException, ClassNotFoundException;
+
+    T search(ID id)throws SQLException,ClassNotFoundException;
+
+    boolean exist(ID id) throws SQLException, ClassNotFoundException;
+
+    boolean delete(ID id) throws SQLException, ClassNotFoundException;
+
+    String generateNewID() throws SQLException, ClassNotFoundException;
+}
