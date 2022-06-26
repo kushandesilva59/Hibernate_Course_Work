f3c947f97d1d6808ecf38bddde33ef7c679a525e
kushandesilva59 <kushandesilva801@gmail.com>
2022-06-26 20:53:59 +0530

    create add room form

---
+++ src/Controller/AddRoomFormController.java
@@ -0,0 +1,21 @@
+package Controller;
+
+import Util.FactoryConfiguration;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class AddRoomFormController {
+    public AnchorPane addRoomContext;
+    public TextField txtType;
+    public TextField txtKeyMoney;
+    public TextField txtQty;
+
+    public void addOnAction(ActionEvent event) {
+
+    }
+
+    public void cancelOnAction(ActionEvent event) {
+
+    }
+}
--- src/Controller/MainFormController.java
+++ src/Controller/MainFormController.java
@@ -34,6 +34,6 @@ public class MainFormController {
     }
 
     public void addRoomOnAction(ActionEvent event) {
-        
+
     }
 }
